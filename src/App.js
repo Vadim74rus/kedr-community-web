53a7b36a7442236794496ef8d82687ed1a3e90f0
Вадим <matveevvs174rus@gmail.com>
2024-07-07 07:05:08 +0500

    init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import './App.css';
-import { useEffect} from "react";
+import React, { useState, useEffect } from 'react';
 import { useTelegram } from "./hooks/useTelegram";
 import Header from "./components/Header/Header";
 
 function App() {
   const { onToggleButton, tg } = useTelegram();
+  const [count, setCount] = useState(0);
+  const [isCounting, setIsCounting] = useState(false);
+
+  const startCounting = () => {
+    if (!isCounting) {
+      setIsCounting(true);
+      let increment = 0.0001;
+      const intervalId = setInterval(() => {
+        setCount((prevCount) => prevCount + increment);
+      }, 30);
+      setTimeout(() => {
+        clearInterval(intervalId);
+        setIsCounting(false);
+      }, 10000);
+    }
+  };
 
   useEffect(() => {
     tg.ready();
@@ -13,6 +28,10 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <h1>K: {count.toFixed(4)}</h1>
+      <button onClick={startCounting} disabled={isCounting}>
+        {isCounting ? "Counting..." : "Start"}
+      </button>
       <button onClick={onToggleButton}>Toggle</button>
     </div>
   );
