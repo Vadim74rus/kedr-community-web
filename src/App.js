0ec09ebc87f2c6413cc9ba5e1bdb1b83906e0377
Вадим <matveevvs174rus@gmail.com>
2024-07-07 07:19:37 +0500

    init

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useTelegram } from "./hooks/useTelegram";
 import Header from "./components/Header/Header";
+import './App.css';
 
 function App() {
   const { onToggleButton, tg } = useTelegram();
@@ -28,11 +29,17 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <h1>K: {count.toFixed(4)}</h1>
-      <button onClick={startCounting} disabled={isCounting}>
-        {isCounting ? "Counting..." : "Start"}
-      </button>
-      <button onClick={onToggleButton}>Toggle</button>
+      <h1 className="count" style={{ marginTop: '150px' }}>
+        K: {count.toFixed(4)}
+      </h1>
+      <div className="button-container">
+        <button className="start-button" onClick={startCounting} disabled={isCounting}>
+          {isCounting ? "Counting..." : "Start"}
+        </button>
+        <button className="toggle-button" onClick={onToggleButton}>
+          Toggle
+        </button>
+      </div>
     </div>
   );
 }
