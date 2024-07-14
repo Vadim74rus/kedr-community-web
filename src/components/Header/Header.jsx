2d953f3acbe5d6f2c4843ea84f7f3644f43de13b
Вадим <matveevvs174rus@gmail.com>
2024-07-15 01:37:44 +0500

    init

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,20 +3,11 @@ import { useTelegram } from "../../hooks/useTelegram";
 
 const Header = () => {
   const { user, onClose } = useTelegram();
-  const receiver = user?.receiver;
 
   return (
     <div className="header">
-      {receiver && (
-        <div className="receiver-info">
-          <h2>Receiver Info:</h2>
-          <p>ID: {receiver.id}</p>
-          <p>Name: {receiver.name}</p>
-          {/* Выводите другие данные о получателе, если они доступны */}
-        </div>
-      )}
       <span className="username">
-        {user?.nickname}
+        {user?.receiver}
       </span>
     </div>
   );
